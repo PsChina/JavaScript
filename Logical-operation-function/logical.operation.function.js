737ac92e6d391977c96cfab751ee487f92ebbe0a
PsChina <panshanzxcv.love@163.com>
2019-04-09 14:15:26 +0800

    简单逻辑函数

--- Logical-operation-function/logical.operation.function.js
+++ Logical-operation-function/logical.operation.function.js
@@ -23,37 +23,13 @@ const add = function(a){
 const intersection = function(a){
     return function(b){
         if(a instanceof Array){
-            // 定义一个空数res组作为返回值的容器，基本操作次数1。
-            const res = []
-            // 定义一个对象用于装数组一的元素，基本操作次数1。
-            const objectA = {}
-            // 使用对象的 hash table 存储元素，并且去重。基本操作次数2n。
-            for(const ele of a) { // 取出n个元素n次
-                objectA[ele] = undefined // 存入n个元素n次
-            }
-            // 定义一个对象用于装数组二的元素，基本操作次数1。
-            const objectB = {}
-            // 使用对象的 hash table 存储元素，并且去重。基本操作次数2n。
-            for(const ele of b){ // 取出n个元素n次
-                objectB[ele] = undefined // 存入n个元素n次
-            }
-            // 使用对象的 hash table 删除相同元素。基本操作次数4n。
-            for(const key in objectA){ //取出n个key (n次操作)
-                if(key in objectB){ // 基本操作1次 (外层循环n次)
-                    delete objectB[key] // 基本操作1次 (外层循环n次)
-                    delete objectA[key] // 基本操作1次 (外层循环n次)（总共是3n 加上n次取key的操作 一共是4n）
+            const newSet = new Set()
+            for(const item of a){
+                if(b.has(item)){
+                    newSet.add(item)
                 }
             }
-            // 将第一个对象剩下来的key push到res容器中，基本操作次数是3n次(最糟糕的情况)。
-            for(const key in objectA){ // 取出n个元素n次(最糟糕的情况)。
-                res[res.length] = key // 读取n次length n次，存入n个元素n次，一共2n(最糟糕的情况)。
-            }
-            // 将第二个对象剩下来的key push到res容器中，基本操作次数也是3n次(最糟糕的情况)。
-            for(const key in objectB){ // 取出n个元素n次(最糟糕的情况)。
-                res[res.length] = key // 读取n次length n次，存入n个元素n次，一共2n(最糟糕的情况)。
-            }
-            // 返回结果，基本操作次数1。
-            return res
+            return [...newSet]
         } else if(a instanceof Set){ // week set and map week map to do
             const newSet = new Set()
             for(const item of a){
