9ad8da19d02e14764463095537974e6b9f5f37d3
PsChina <panshanzxcv.love@163.com>
2020-03-24 16:01:32 +0800

    MyPromise

---
+++ Promise.js
@@ -0,0 +1,25 @@
+function MyPromise(func) {
+    const _this = this;
+    _this.onResolveCallBack = [];
+    function resolve(data) {
+        _this.onResolveCallBack.forEach((_func) => {
+            _func(data);
+        })
+    }
+    func(resolve.bind(_this));
+}
+
+MyPromise.prototype.then = function (func) {
+    const _this = this;
+    return new MyPromise((resolve) => {
+        _this.onResolveCallBack.push((data) => {
+            var res = func(data);
+            if (res instanceof MyPromise) {
+                res.then(resolve);
+            } else {
+                resolve(res);
+            }
+        })
+    });
+}
+
