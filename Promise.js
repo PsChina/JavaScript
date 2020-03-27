3752d27a86955b19b18dbf9e6e90f9ee0e7343a1
PsChina <panshanzxcv.love@163.com>
2020-03-27 14:45:43 +0800

    Add the class notation.

--- Promise.js
+++ Promise.js
@@ -23,3 +23,27 @@ MyPromise.prototype.then = function (func) {
     });
 }
 
+class PromiseB {
+    constructor(func){
+        this.resolveCallBacks = [];
+        const resolve = (res)=>{
+            this.resolveCallBacks.forEach((each_resolve)=>{
+                each_resolve(res);
+            })
+        }
+        func(resolve.bind(this));
+    }
+    then(func){
+        return new this.constructor((resolve)=>{
+            this.resolveCallBacks.push((data)=>{
+                const res = func(data);
+                if(res instanceof this.constructor){
+                    res.then(resolve);
+                } else {
+                    resolve(res);
+                }                
+            })
+        })
+    }
+}
+
