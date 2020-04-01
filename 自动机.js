410abb7165eaf75739eeaea07c2bd158a8f4c03c
PsChina <panshanzxcv.love@163.com>
2020-04-01 21:25:45 +0800

    char is in

--- 自动机.js
+++ 自动机.js
@@ -34,7 +34,29 @@ let helloParserFac = (next) => char('h', char('e', char('l', char('l', char('o',
 let helloParser = helloParserFac(ok)
 let hello2020 = helloParserFac(char(' ', digit(digit(digit(digit())))))
 
-console.log(char('h')('hello'))
-console.log(helloParser('hello'))
-console.log(helloParser('hello, world'))
-console.log(hello2020('hello 2020'))
+// console.log(char('h')('hello'))
+// console.log(helloParser('hello'))
+// console.log(helloParser('hello, world'))
+// console.log(hello2020('hello 2020'))
+
+
+function charIsIn(c, cb = ok) {
+    return isInCond((s) => s === c, c, cb)
+}
+
+function isInCond(p, c, cb = cond) {
+    return function (s) {
+        if (s.length === 0) {
+            return undefined;
+        }
+        const index = s.indexOf(c);
+        if (index > -1) {
+            const res = s.slice(index);
+            return [s.slice(0, index), ...cond(p, cb)(res)];
+        }
+    }
+}
+
+console.log(charIsIn('w')('Hello world'))
+
+
