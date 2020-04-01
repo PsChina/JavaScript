643a45b2582a259819eff7a76b2e3fe8aaba92a9
PsChina <panshanzxcv.love@163.com>
2020-04-01 20:04:31 +0800

    自动机

---
+++ 自动机.js
@@ -0,0 +1,40 @@
+
+// 终止状态
+function ok(s) {
+    return ['', s]
+}
+
+// 构造一个p自动机结点
+function cond(p, cb = ok) {
+    return function (s) {
+        if (s.length === 0) {
+            return undefined;
+        }
+        if (p(s[0])) {
+            let res = cb(s.slice(1));
+            if (!res) return undefined;
+            return [s[0] + res[0], res[1]];
+        }
+        return undefined;
+    }
+}
+
+// 如果当前字符串前缀为c，就接受
+function char(c, cb = ok) {
+    return cond((s) => s === c, cb)
+}
+
+// 如果当前字符串前缀是数字，就接受
+function digit(cb = ok) {
+    return cond((s) => '0' <= s && s <= '9', cb)
+}
+
+// 识别 'hello'
+let helloParserFac = (next) => char('h', char('e', char('l', char('l', char('o', next)))))
+let helloParser = helloParserFac(ok)
+let hello2020 = helloParserFac(char(' ', digit(digit(digit(digit())))))
+
+console.log(char('h')('hello'))
+console.log(helloParser('hello'))
+console.log(helloParser('hello, world'))
+console.log(hello2020('hello 2020'))
