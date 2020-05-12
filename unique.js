737533094d3d29cae7b73b4852a4e895dc01a0e2
PsChina <panshanzxcv.love@163.com>
2020-05-12 10:25:22 +0800

    相邻不等去重法

---
+++ unique.js
@@ -0,0 +1,17 @@
+// 相邻不等去重法
+function unique(arr) {
+    arr.sort();
+    const { length } = arr;
+    const newArr = [arr[0]]
+    for (let i = 1; i < length; i++) {
+        const A = arr[i];
+        const B = arr[i - 1];
+        if (A !== B) {
+            if (A !== A && B !== B) {
+                continue
+            }
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+}
