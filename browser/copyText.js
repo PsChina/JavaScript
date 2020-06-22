04b6b5dd528cf786e21cc3b9d39149005ead5e4a
PsChina <panshanzxcv.love@163.com>
2020-06-22 14:27:04 +0800

    update copy text

--- browser/copyText.js
+++ browser/copyText.js
@@ -1,11 +1,11 @@
-function copyText(text) {
+export function copyText(text) {
     const textarea = document.createElement('textarea');
     textarea.value = text;
-    textarea.style.width = '0';
-    textarea.style.height = '0';
     textarea.style.padding = '0';
     textarea.style.margin = '0';
-    textarea.style.border = 'none';
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+    textarea.style.opacity = '0';
     document.body.append(textarea);
     textarea.select();
     document.execCommand('Copy');
