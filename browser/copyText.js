dcfaf51cc39ca496e943a6bc17861434e11706de
PsChina <panshanzxcv.love@163.com>
2020-06-22 12:18:16 +0800

    update copy text

--- browser/copyText.js
+++ browser/copyText.js
@@ -1,12 +1,11 @@
 function copyText(text) {
-    var textarea = document.createElement('textarea');
+    const textarea = document.createElement('textarea');
     textarea.value = text;
-    textarea.style.width = 0;
-    textarea.style.height = 0;
-    textarea.style.padding = 0;
-    textarea.style.margin = 0;
+    textarea.style.width = '0';
+    textarea.style.height = '0';
+    textarea.style.padding = '0';
+    textarea.style.margin = '0';
     textarea.style.border = 'none';
-    textarea.zIndex = -9999;
     document.body.append(textarea);
     textarea.select();
     document.execCommand('Copy');
