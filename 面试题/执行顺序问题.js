424a7c90d28ecbfa5ac309d2f68e315546ab993e
PsChina <panshanzxcv.love@163.com>
2020-11-18 13:57:08 +0800

    添加执行顺序面试题

---
+++ 面试题/执行顺序问题.js
@@ -0,0 +1,42 @@
+(function () {
+
+    setTimeout(() => {
+        console.log('timeout 0');
+    }, 0);
+
+    let i = 0;
+
+    for (; i < 3; i++) {
+        p[i] = function () {
+            return new Promise(function (resolve) {
+                console.log(`promise ${i}`);
+                resolve(`promsie ${i * i}`);
+            })
+        }
+    }
+
+})();
+
+async function b() {
+
+    console.log('async -1');
+
+}
+
+function a() {
+
+    console.log(`async ${p.length}`);
+
+    return async function () {
+        console.log(`async ${p.length}`);
+        await b();
+        console.log('async -2');
+    }
+
+}
+
+p.push(a());
+
+p[1]().then(console.log);
+
+p[3]();
