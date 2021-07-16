62064b07c9a2e196776b62672ce7a2a086027e1a
PsChina <panshanzxcv.love@163.com>
2021-07-16 23:20:43 +0800

    update promise

--- Promise.js
+++ Promise.js
@@ -1,49 +1,95 @@
-function MyPromise(func) {
-    const _this = this;
-    _this.onResolveCallBack = [];
-    function resolve(data) {
-        _this.onResolveCallBack.forEach((_func) => {
-            _func(data);
-        })
-    }
-    func(resolve.bind(_this));
-}
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
 
-MyPromise.prototype.then = function (func) {
-    const _this = this;
-    return new MyPromise((resolve) => {
-        _this.onResolveCallBack.push((data) => {
-            const res = func(data);
-            if (res instanceof MyPromise) {
-                res.then(resolve);
-            } else {
-                resolve(res);
+class MyPromise {
+    status = PENDING
+    onFulFilledCallbacks = []
+    onRejctedCallbacks = []
+    value = null
+    reason = null
+    constructor(fn) {
+        try {
+            fn(this.resolve.bind(this), this.reject.bind(this));
+        } catch (e) {
+            this.reject(e)
+        }
+    }
+    resolve(value) {
+        if (this.status === PENDING) {
+            this.status = FULFILLED
+            this.value = value
+            while (this.onFulFilledCallbacks.length) {
+                this.onFulFilledCallbacks.shift()(value)
+            }
+        }
+    }
+    reject(reason) {
+        if (this.status === PENDING) {
+            this.status = REJECTED
+            this.reason = reason
+            while (this.onRejctedCallbacks.length) {
+                this.onRejctedCallbacks.shift()(reason)
+            }
+        }
+    }
+    then(onFulfilled = value => value, onRejected = reason => { throw reason }) {
+
+        const promise = new MyPromise((resolve, reject) => {
+            if (this.status === FULFILLED) {
+                queueMicrotask(() => {
+                    try {
+                        const x = onFulfilled(this.value)
+                        resolvePromise(x, promise, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+            } else if (this.status === REJECTED) {
+                let callBack
+                if (onRejected instanceof Function) {
+                    callBack = onRejected
+                } else if (onFulfilled instanceof Function) {
+                    callBack = onFulfilled
+                }
+                queueMicrotask(() => {
+                    try {
+                        const x = callBack(this.reason)
+                        resolvePromise(x, promise, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+            } else if (this.status === PENDING) {
+                this.onFulFilledCallbacks.push(() => {
+                    queueMicrotask(() => {
+                        try {
+                            const x = onFulfilled(this.value)
+                            resolvePromise(x, promise, resolve, reject)
+                        } catch (e) {
+                            reject(e)
+                        }
+                    })
+                })
+                let callBack
+                if (onRejected) {
+                    callBack = onRejected
+                } else {
+                    callBack = onFulfilled
+                }
+                this.onRejctedCallbacks.push(() => {
+                    queueMicrotask(() => {
+                        try {
+                            const x = callBack(this.reason)
+                            resolvePromise(x, promise, resolve, reject)
+                        } catch (e) {
+                            reject(e)
+                        }
+                    })
+                })
             }
         })
-    });
-}
 
-class PromiseB {
-    constructor(func){
-        this.resolveCallBacks = [];
-        const resolve = (res)=>{
-            this.resolveCallBacks.forEach((each_resolve)=>{
-                each_resolve(res);
-            })
-        }
-        func(resolve.bind(this));
-    }
-    then(func){
-        return new this.constructor((resolve)=>{
-            this.resolveCallBacks.push((data)=>{
-                const res = func(data);
-                if(res instanceof this.constructor){
-                    res.then(resolve);
-                } else {
-                    resolve(res);
-                }                
-            })
-        })
+        return promise
     }
 }
-
